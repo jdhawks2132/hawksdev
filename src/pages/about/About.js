18faea028e58126d78d47f8d315fae4c1962f1f5
Joshua Hawks <jdhawks@gmail.com>
2022-07-18 09:50:21 -0500

    adding teaching paragraph to about

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -13,8 +13,16 @@ function About() {
 			<div className='about-content'>
 				<p>
 					Full-stack Software Engineer at the Texas School Safety Center with a
-					background in education. As a Full-stack Developer, I love
-					the creative process behind design and problem solving.
+					background in education. As a Full-stack Developer, I love the
+					creative process behind design and problem solving.
+				</p>
+				<p>
+					I am passionate about helping others learn to code have served as a
+					Cohort Team Lead for the Dallas FreeCodeCamp group. Our cohorts work
+					on small projects throughout the year to offer growing developers a
+					chance to work and learn in a collaborative environment. In addition
+					to FCC, I am also a teaching assistant at Washington State University
+					in St. Louis' Software Engineering Bootcamp.
 				</p>
 				<p>
 					I am a freelance classical musician (oboe and bassoon) and graphic
